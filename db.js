44ac4f9218b77e9cb6bc8f8c5d76f5eab10a7b4a
chris097 <christian@esettlementgroup.com>
2022-03-31 16:48:46 +0100

    change database

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     Host: "ec2-52-3-60-53.compute-1.amazonaws.com",
-    Database: "d11aas1v4qp1b1",
-    User: "mxkmdvvewcdvln",
+    Database: "dfokb2n758frv0",
+    User: "zvngvoducmidpl",
     Port: 5432,
-    Password: "1f267c59ec5cf488d445fcac4687dee7efffbe7e6c9a20ca7a40f5519f06aa3a",
+    Password: "7413c7e75421408c8b5d9ff865aaa7612662d06e0ddb8976bf3a7ad6564aef11",
 });
 
 module.exports = pool;
