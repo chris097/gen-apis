0c359d7fa49be8a18ecf47c9fca2fd07a5774d96
chris097 <christian@esettlementgroup.com>
2022-03-31 09:36:10 +0100

    fixing bug

--- src/students/controller.js
+++ src/students/controller.js
@@ -2,10 +2,7 @@ const pool = require('../../db');
 const queries = require('../students/queries');
 
 const getStudents = (req, res) => {
-    pool.query(queries.getStudents, (error, results) => {
-        if(error) throw error;
-        res.status(200).json(results.rows)
-    })
+    res.send("Get all  students.")
 };
 
 module.exports = {
