35edba573ec4d6485154de2515ecb28ce8024d23
chris097 <christian@esettlementgroup.com>
2022-03-31 09:03:24 +0100

    connected to database

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const studentsRouter = require('./src/students/routes');
+
 const app = express();
 const port = 4040;
 
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
+
+app.use('/api/v1/students', studentsRouter)
 
 app.listen(process.env.PORT || port, 
     () => console.log(`Connect to port ${port}`))
