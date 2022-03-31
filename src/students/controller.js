6c34c5a745dedd6678fa86de414836a2a5fbffa5
chris097 <christian@esettlementgroup.com>
2022-03-31 16:44:35 +0100

    fix bug

--- src/students/controller.js
+++ src/students/controller.js
@@ -3,12 +3,8 @@ const queries = require('../students/queries');
 
 const getStudents = (req, res) => {
     pool.query(queries.getStudents, (error, results) => {
-        // if(error) throw error;
-        if(results?.rows === []){
-            res.send('empty')
-        }else{
-            res.status(200).json(results?.rows)
-        }
+        res.status(200).json(results?.rows);
+        if(error) throw error;
     })
 };
 
