6d7cad71a8ca669d6ea119d1f51fd8056994cebf
chris097 <christian@esettlementgroup.com>
2022-03-31 16:39:34 +0100

    fix bug

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "mxkmdvvewcdvln",
-    host: "ec2-52-3-60-53.compute-1.amazonaws.com",
-    database: "d11aas1v4qp1b1",
-    password: "1f267c59ec5cf488d445fcac4687dee7efffbe7e6c9a20ca7a40f5519f06aa3a",
-    port: 5432
+    Host: "ec2-52-3-60-53.compute-1.amazonaws.com",
+    Database: "d11aas1v4qp1b1",
+    User: "mxkmdvvewcdvln",
+    Port: 5432,
+    Password: "1f267c59ec5cf488d445fcac4687dee7efffbe7e6c9a20ca7a40f5519f06aa3a",
 });
 
 module.exports = pool;
--- src/students/controller.js
+++ src/students/controller.js
@@ -4,7 +4,11 @@ const queries = require('../students/queries');
 const getStudents = (req, res) => {
     pool.query(queries.getStudents, (error, results) => {
         if(error) throw error;
-        res.status(200).json(results.rows)
+        if(results?.rows === []){
+            res.send('empty')
+        }else{
+            res.status(200).json(results?.rows)
+        }
     })
 };
 
