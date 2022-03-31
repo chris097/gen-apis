7a0630081c79ff439550015c7c5b5a4c652fa4c7
chris097 <christian@esettlementgroup.com>
2022-03-31 09:12:21 +0100

    fix bug

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const studentsRouter = require('./src/students/routes');
 const app = express();
 const port = 4040;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
--- src/students/controller.js
+++ src/students/controller.js
@@ -3,7 +3,6 @@ const queries = require('../students/queries');
 
 const getStudents = (req, res) => {
     pool.query(queries.getStudents, (error, results) => {
-        if(error) throw error;
         res.send(results.rows)
     })
 };
