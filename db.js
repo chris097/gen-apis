3a9534cb34482036be49ba5b2f9c601b99aa9364
chris097 <christian@esettlementgroup.com>
2022-03-31 16:30:04 +0100

    connected database to heroku

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "students",
+    user: "mxkmdvvewcdvln",
+    host: "ec2-52-3-60-53.compute-1.amazonaws.com",
+    database: "d11aas1v4qp1b1",
+    password: "1f267c59ec5cf488d445fcac4687dee7efffbe7e6c9a20ca7a40f5519f06aa3a",
     port: 5432
 });
 
